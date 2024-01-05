03c106265e6aca13b56928290332c3ce62c75059
tatitanizaki <tatiana.tanizaki@gmail.com>
2024-01-05 10:23:08 -0500

    working until this point

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,13 +58,13 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <span style={{ verticalAlign: 'middle', fontSize: '1.25rem', paddingLeft: '1rem'}}> £</span>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '1rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,13 +4,23 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const [error, setError] = useState('');
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const value = Number(event.target.value);
+        if (value > 20000) {
+            alert('The value cannot exceed 20,000');
+        } else if (value < 1660) {
+            alert('You cannot reduce the budget value lower than the spending');
+        } else {
+            setError('');
+            setNewBudget(value);
+        }
     }
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £ </span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange} max="20000"></input>
+{error && <div className="alert alert-danger mt-2">{error}</div>}
 </div>
     );
 };
